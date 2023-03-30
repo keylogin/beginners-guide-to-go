8b9ba1a21adeb7670d4049aef41cf33d8d83b87d
Keylogin <seriousam111@gmail.com>
2023-03-30 13:14:46 +0300

    update

--- 07-arrays-and-slices-in-go/main.go
+++ 07-arrays-and-slices-in-go/main.go
@@ -8,15 +8,17 @@ func main() {
 	// [1, 2, 3, 4]
 	planets := [8]string{"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "nepture"}
 	fmt.Println(planets)
+	fmt.Println(planets[0:])
 
-	var planetsArray [8]string
-	planetsArray[0] = "mercury"
+	var planetsArray [8]bool
+	planetsArray[0] = true
 	fmt.Println(planetsArray)
 
 	planetSlice := []string{"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "nepture"}
 	fmt.Println(planetSlice)
+	fmt.Println(planetSlice[4])
 
 	var planetSliceVerbose []string
-	planetSliceVerbose = append(planetSliceVerbose, "mercury")
+	planetSliceVerbose = append(planetSliceVerbose, "newera")
 	fmt.Println(planetSliceVerbose)
 }
